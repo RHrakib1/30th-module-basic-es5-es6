cf59c9fae1c0eabdea42644d613699e6a3d66071
RHrakib1 <rakibhasanmd457@gmail.com>
2024-02-23 22:29:26 +0600

    add to the task

--- task.js
+++ task.js
@@ -2,3 +2,9 @@
 const multiplay = (x, y, z) => (x * y * z);
 const multiStore = (2 * 2 * 5);
 console.log(multiStore);
+
+// template string 
+const wordName=`I Love biriany.
+i love codding.
+i love tarin.`
+console.log(wordName)
